817d7abd9cd4d0d9566d260a637eb6b77dd21bde
Kenta Mori <morinpic0518@gmail.com>
2019-12-15 12:53:21 +0900

    Feature/update 201912 (#2)
    * Modify defaults開始前後にログを表示
    
    * Update README

--- setup.sh
+++ setup.sh
@@ -1,32 +1,7 @@
 #!/bin/sh
 
-# Command Line Toolsをインストール
-xcode-select --install
-
-# Homebrewのインストール
-if [ ! -x "`which brew`" ]; then
-  echo "start install and update brew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew update
-  brew upgrade
-  brew cleanup
-  brew -v
-fi
-
-# brewfileを.brewfileとしてmackupで管理しているためmackupを先にインストールする
-brew install mackup
-brew install dropbox
-
-# AppStoreとDropboxへのログインを促す
-read -p "Please log in to Appstore & Dropbox!!"
-
-# mackupでリストアする
-mackup restore
-
-# Homebrew Packageのインストール
-brew bundle install --file=.brewfile
-
 # ==== システム環境設定 ====
+echo "Start system environment settings"
 # Dockを自動的に隠す
 defaults write com.apple.dock autohide -bool true
 # カーソルの移動速度を変更 （1〜15）
@@ -46,3 +21,31 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # USBやネットワークストレージに.DS_Storeファイルを作成しない
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+echo "End system environment settings"
+
+# Command Line Toolsをインストール
+xcode-select --install
+
+# Homebrewのインストール
+if [ ! -x "`which brew`" ]; then
+  echo "start install and update brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+  brew upgrade
+  brew cleanup
+  brew -v
+fi
+
+# brewfileを.brewfileとしてmackupで管理しているためmackupを先にインストールする
+brew install mackup
+# mackupのbackup先はドロップボックスなので先にインストールしておく
+brew cask install dropbox
+
+# AppStoreとDropboxへのログインを促す
+read -p "Please log in to Appstore & Dropbox!!"
+
+# mackupでリストアする
+mackup restore
+
+# Homebrew Packageのインストール
+brew bundle install --file=.brewfile
